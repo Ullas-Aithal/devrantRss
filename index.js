01f470a1cf441f7fb5e30d017a76b9c5e2fafdb3
Ullas-Aithal <ullasaithalk@gmail.com>
2019-04-01 21:26:17 -0500

    Completed code to pull rants and write to response as xml

--- index.js
+++ index.js
@@ -5,97 +5,127 @@ var moment = require('moment');
 var cors = require('cors');
 var app = express();
 var RSS = require('rss');
-var tempResponse = {
-    "success": true,
-    "rants": [
-      {
-        "id": 2046279,
-        "text": "Where's my caviar",
-        "score": 62,
-        "created_time": 1553920784,
-        "attached_image": {
-          "url": "https://img.devrant.com/devrant/rant/r_2046279_6FTxJ.jpg",
-          "width": 640,
-          "height": 465
-        },
-        "num_comments": 8,
-        "tags": [
-          "joke/meme",
-          "legend",
-          "if statements",
-          "conditions"
-        ],
-        "vote_state": 0,
-        "edited": false,
-        "rt": 1,
-        "rc": 3,
-        "user_id": 2030717,
-        "user_username": "iampoppyxx",
-        "user_score": 365,
-        "user_avatar": {
-          "b": "7bc8a4",
-          "i": "v-21_c-3_b-1_g-m_9-1_1-4_16-12_3-1_8-1_7-1_5-1_12-4_6-102_2-1_22-1_11-1_18-1_19-1_4-1.jpg"
-        },
-        "user_avatar_lg": {
-          "b": "7bc8a4",
-          "i": "v-21_c-1_b-1_g-m_9-1_1-4_16-12_3-1_8-1_7-1_5-1_12-4_6-102_2-1_22-1_11-1_18-1_19-1_4-1.png"
-        }
-      }]
-  }
-  
-app.use(bodyParser.json());
-app.use(cors());
+var portNumber = 800;
+var devRantAPI = 'https://devrant.com/api/devrant/rants?app=3&sort=top&range=day&limit=20&skip=0';
 var feedOptions = {
-    title:'DevRant unofficial RSS',
+    title:'DevRant Unofficial RSS',
     feed_url: 'https://example.com',
     site_url: 'https://devrant.com',
 }
-var tags = tempResponse.rants[0].tags;
-if(tags.length > 0){
-    tags = tags.join(', ');
-}
-var itemOptions = {
-    title: 'Rant from ' + tempResponse.rants[0].user_username,
-    description: '<h4>' + tempResponse.rants[0].text + '</h4>' + 
-                '<a href="' + tempResponse.rants[0].attached_image.url + '" target="_blank"><img src="'+ tempResponse.rants[0].attached_image.url +'" height="'+ tempResponse.rants[0].attached_image.height +'" width="' + tempResponse.rants[0].attached_image.width + '" /></a>' + 
-                '<h5>' + tempResponse.rants[0].score + ' ++s | '+ tempResponse.rants[0].num_comments + ' comments </h5> ' +
-                '<p> Tags: <i>' + tags + '</i></p>' +
-                '<p> User profile: <a href="https://devrant.com/users/'+ tempResponse.rants[0].user_username +'" target="_blank">'+ tempResponse.rants[0].user_username +'</a></p>' +
-                '<p> Rant link: <a href="https://devrant.com/rants/'+ tempResponse.rants[0].id +'" target="_blank">https://devrant.com/rants/'+ tempResponse.rants[0].id +'</a></p>',
-    url: 'https://devrant.com/rants/' + tempResponse.rants[0].id, // link to the item
-    guid: tempResponse.rants[0].created_time,
-    date: moment.unix(tempResponse.rants[0].created_time).toDate(),
-}
-var feed = new RSS(feedOptions);
-feed.item(itemOptions);
-var xml = feed.xml({indent: true});
+app.use(bodyParser.json());
+app.use(cors());
 
-
-
-app.get('/',(req,response) => {
-    console.log(xml);
-    response.send(xml);
+app.get('/getrss',(req,res) => {
+          buildFeed(res);
 });
 
-app.get('/rss',(req,res) => {
-    var options = {
-        url:'https://devrant.com/api/devrant/rants?app=3&sort=top&range=day&limit=20&skip=0',
-        method: 'GET',
-        
-      }    
-    request(options,(error,response,body)=>{
-        console.log(options.url);
-        if(error){
-            console.log("Error connecting " + error);
+app.listen(portNumber,() => {
+    console.log('Listening on ' + portNumber);
+});
+
+/**
+ * This function gets the rants, creates a feed and writes the xml to the response
+ * @param {response} res - Response for /getrss 
+ */
+function buildFeed(res){
+    //Get rants
+    getRants().then((rants)=>{
+        //Build feed by adding rants as item to feed
+        buildAndAddToFeed(rants).then((xml)=>{
+            //Send response as rss xml
+            res.send(xml);
+        },(error) => {
             res.send(error);
+        })
+    }, (error) => {
+        res.send(error);
+    });
+}
+/**
+ * This function gets the rants from devrant and returns a promise 
+ * API call - https://devrant-docs.github.io/
+ */
+function getRants(){
+    var options = {
+        url: devRantAPI,
+        method: 'GET',        
+    }
+    return new Promise((resolve, reject)=>{   
+        request(options,(error,response,body)=>{
+            if(error){
+                reject(error);
+            }
+            else{
+                var result = JSON.parse(body);
+                //If 'success' type is undefined, then we didn't get the right response
+                if(result.success === undefined){
+                    reject("Error getting rants. 'success' parameter in response is undefined");
+                }
+                //If 'success' type is false, then we are not calling the api right
+                if(!result.success){
+                    reject("Error getting rants. 'success' parameter in response is undefined");
+                }
+                //No 'rants' object. Nothing to do
+                if(result.rants === undefined){
+                    reject("rants object in response is undefined");
+                }
+                else{
+                    //Return rants
+                    resolve(result.rants);
+                }
+            }
+        });
+    })
+}
+
+/**
+ * This function takes the rants, loops over each rant, adds most of the information as HTML and returns the XML
+ * @param {Object} rants 
+ */
+function buildAndAddToFeed(rants){
+    return new Promise((resolve,reject) => {  
+        //No rants    
+        if(rants.length <=0 ){
+            reject("No rants");
         }
         else{
-            console.log(response.statusCode + ' ' + response.statusMessage + ' for: ' + options.url);
-            res.send(body);
+            //Create new RSS feed
+            var feed = new RSS(feedOptions);
+            //For each rant
+            for(var index in rants){
+                var itemOptions = {};
+                //Rant title which will be username
+                itemOptions.title = 'Rant from ' + rants[index].user_username;
+                //Start building description as HTML
+                itemOptions.description = '<h4>' + rants[index].text + '</h4>'; 
+                //Add image to the HTML only if it exists
+                if(rants[index].attached_image != ""){
+                    itemOptions.description += '<a href="' + rants[index].attached_image.url + '" target="_blank"><img src="'+ rants[index].attached_image.url +'" height="'+ rants[index].attached_image.height +'" width="' + rants[index].attached_image.width + '" /></a>';
+                }
+                itemOptions.description += '<h5>' + rants[index].score + ' ++s | '+ rants[index].num_comments + ' comments </h5> ';
+                if(rants[index].tags != ""){
+                    var tags = rants[index].tags;
+                    if(tags.length > 0){
+                        tags = tags.join(', ');
+                        itemOptions.description += '<p> Tags: <i>' + tags + '</i></p>';
+                    }                
+                }
+                //Link to user profile
+                itemOptions.description += '<p> User profile: <a href="https://devrant.com/users/'+ rants[index].user_username +'" target="_blank">'+ rants[index].user_username +'</a></p>';
+                //Link to the rant
+                itemOptions.description += '<p> Rant link: <a href="https://devrant.com/rants/'+ rants[index].id +'" target="_blank">https://devrant.com/rants/'+ rants[index].id +'</a></p>';
+                //Link to the rant as a separate variable
+                itemOptions.url = 'https://devrant.com/rants/' + rants[index].id;
+                //Unique id which will be the created time which will be unique
+                itemOptions.guid = rants[index].created_time;
+                //Conver unix time to date object
+                itemOptions.date = moment.unix(rants[index].created_time).toDate();
+                //Add item to feed
+                feed.item(itemOptions);
+            }
+            //Send xml
+            resolve(feed.xml({indent: true}));
         }
-    })   
-});
+    });
+}
 
-app.listen(800,() => {
-    console.log('Listening on 800');
-});
